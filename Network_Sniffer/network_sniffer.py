b2c30ba4b36ec08a867c97404b263c1fc7223bcf
Ankit Halder <halderankit9@gmail.com>
2024-11-16 09:49:02 +0530

    working program
    successfully (basic) program is able to sniff network

--- Network_Sniffer/network_sniffer.py
+++ Network_Sniffer/network_sniffer.py
@@ -1,5 +1,40 @@
-# ==================================================#
+# ==================================================
 # I am going to use scapy packag to perform sniffing
-# ==================================================#
+# ==================================================
 
-from scapy.all import sniff, IP, TCP, UDP, Ether
+# ===================headers========================
+from scapy.all import *
+from scapy.layers.inet import IP
+from scapy.layers.http import HTTPRequest, TCP
+from colorama import init, Fore
+
+# =================color=declaration================
+init()
+
+r = Fore.RED        #RED
+g = Fore.GREEN      #GREEN
+bu = Fore.BLUE      #BLUE
+y = Fore.YELLOW     #YELLOW
+c = Fore.CYAN       #CYAN
+rst = Fore.RESET    #RESET color
+
+# ==================================================
+
+
+def sniff_packets(iface):
+    if iface:
+        sniff(prn = prc_packets, iface = iface, store = False)
+    else:
+        sniff(prn = prc_packets, store = False)
+
+def prc_packets(packet):
+
+    if packet.haslayer(TCP):
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        src_port = packet[TCP].sport
+        dst_port = packet[TCP].dport
+
+        print(f"{bu}[+] {src_ip} is using port {src_port} to connect {dst_ip} at port {dst_port} {rst}")
+
+sniff_packets('VMware Network Adapter VMnet8')  #heare we can use any network interface I am doing on VM From windows. so, I use 'VMware Network Adapter VMnet8' name
